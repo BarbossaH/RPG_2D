d534c9e549b7a4ec8821bac80a4cd92e8c1bf642
BarbossaH <huangbobeijing2008@gmail.com>
2024-08-07 19:06:11 +1200

    items

---
+++ Assets/Scripts/Inventory/ItemWeapon.cs
@@ -0,0 +1,9 @@
+
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Items/Weapon", fileName = "ItemWeapon")]
+public class ItemWeapon : InventoryItem
+{
+  [Header("Weapon")]
+  public Weapon weapon;
+}
